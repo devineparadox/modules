319efc4e15c58490970ef62c28dadcf996e058d0
ᴅᴇᴠɪɴᴇ <devineparadoxz@gmail.com>
2025-01-30 22:50:57 +0530

    Create purge.py

---
+++ plugins/purge.py
@@ -0,0 +1,115 @@
+from asyncio import sleep
+
+from pyrogram import filters
+from pyrogram.enums import ChatType
+from pyrogram.errors import MessageDeleteForbidden, RPCError
+from pyrogram.types import Message
+
+from Devine import app
+from Devine.utils.can_restrict import can_restrict
+
+
+@app.on_message(filters.command("purge"))
+@can_restrict
+async def purge(c: app, m: Message):
+    if m.chat.type != ChatType.SUPERGROUP:
+        await m.reply_text(text="ᴄᴀɴɴᴏᴛ ᴘᴜʀɢᴇ ᴍᴇssᴀɢᴇs ɪɴ ᴀ ʙᴀsɪᴄ ɢʀᴏᴜᴘ")
+        return
+
+    if m.reply_to_message:
+        message_ids = list(range(m.reply_to_message.id, m.id))
+
+        def divide_chunks(l: list, n: int = 100):
+            for i in range(0, len(l), n):
+                yield l[i : i + n]
+
+        m_list = list(divide_chunks(message_ids))
+
+        try:
+            for plist in m_list:
+                await c.delete_messages(
+                    chat_id=m.chat.id,
+                    message_ids=plist,
+                    revoke=True,
+                )
+            await m.delete()
+        except MessageDeleteForbidden:
+            await m.reply_text(
+                text="ᴄᴀɴɴᴏᴛ ᴅᴇʟᴇᴛᴇ ᴀʟʟ ᴍᴇssᴀɢᴇs. ᴛʜᴇ ᴍᴇssᴀɢᴇs ᴍᴀʏ ʙᴇ ᴛᴏᴏ ᴏʟᴅ, ɪ ᴍɪɢʜᴛ ɴᴏᴛ ʜᴀᴠᴇ ᴅᴇʟᴇᴛᴇ ʀɪɢʜᴛs, ᴏʀ ᴛʜɪs ᴍᴀʏ ɴᴏᴛ ʙᴇ ᴀ sᴜᴘᴇʀɢʀᴏᴜᴘ."
+            )
+            return
+        except RPCError as ef:
+            await m.reply_text(
+                text=f"""sᴏᴍᴇ ᴇʀʀᴏʀ ᴏᴄᴄᴜʀʀᴇᴅ!
+
+      <b>ᴇʀʀᴏʀ :</b> <code>{ef}</code>"""
+            )
+
+        count_del_msg = len(message_ids)
+
+        z = await m.reply_text(text=f"ᴅᴇʟᴇᴛᴇᴅ <i>{count_del_msg}</i> ᴍᴇssᴀɢᴇs")
+        await sleep(3)
+        await z.delete()
+        return
+    await m.reply_text("ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴍᴇssᴀɢᴇ ᴛᴏ sᴛᴀʀᴛ ᴘᴜʀɢᴇ !")
+    return
+
+
+@app.on_message(filters.command("spurge"))
+@can_restrict
+async def spurge(c: app, m: Message):
+    if m.chat.type != ChatType.SUPERGROUP:
+        await m.reply_text(text="ᴄᴀɴɴᴏᴛ pᴜʀɢᴇ ᴍᴇssᴀɢᴇs ɪɴ ᴀ ʙᴀsɪᴄ ɢʀᴏᴜᴘ")
+        return
+
+    if m.reply_to_message:
+        message_ids = list(range(m.reply_to_message.id, m.id))
+
+        def divide_chunks(l: list, n: int = 100):
+            for i in range(0, len(l), n):
+                yield l[i : i + n]
+
+        m_list = list(divide_chunks(message_ids))
+
+        try:
+            for plist in m_list:
+                await c.delete_messages(
+                    chat_id=m.chat.id,
+                    message_ids=plist,
+                    revoke=True,
+                )
+            await m.delete()
+        except MessageDeleteForbidden:
+            await m.reply_text(
+                text="ᴄᴀɴɴᴏᴛ dᴇʟᴇᴛᴇ ᴀʟʟ ᴍᴇssᴀɢᴇs. ᴛʜᴇ ᴍᴇssᴀɢᴇs ᴍᴀʏ ʙᴇ ᴛᴏᴏ ᴏʟᴅ, ɪ ᴍɪɢʜᴛ ɴᴏᴛ ʜᴀᴠᴇ dᴇʟᴇᴛᴇ ʀɪɢʜᴛs, ᴏʀ ᴛʜɪs ᴍᴀʏ ɴᴏᴛ ʙᴇ ᴀ sᴜᴘᴇʀɢʀᴏᴜᴘ."
+            )
+            return
+        except RPCError as ef:
+            await m.reply_text(
+                text=f"""sᴏᴍᴇ ᴇʀʀᴏʀ ᴏᴄᴄᴜʀʀᴇᴅ, ʀᴇᴘᴏʀᴛ ᴛᴏ @{SUPPORT_CHAT}
+
+      <b>ᴇʀʀᴏʀ:</b> <code>{ef}</code>"""
+            )
+        return
+    await m.reply_text("ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴍᴇssᴀɢᴇ ᴛᴏ sᴛᴀʀᴛ sᴘᴜʀɢᴇ !")
+    return
+
+
+@app.on_message(
+    filters.command("del"),
+    group=9,
+)
+@can_restrict
+async def del_msg(c: app, m: Message):
+    if m.chat.type != ChatType.SUPERGROUP:
+        return
+
+    if m.reply_to_message:
+        await m.delete()
+        await c.delete_messages(
+            chat_id=m.chat.id,
+            message_ids=m.reply_to_message.id,
+        )
+    else:
+        await m.reply_text(text="ᴡʜᴀᴛ ᴅᴏ ʏᴏᴜ ᴡᴀɴɴᴀ ᴅᴇʟᴇᴛᴇ?")
+    return
